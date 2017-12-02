38fe1a1f54a1e19b4ede08c214bad26062f8dba0
flambok <amine@flambok.com>
2017-12-02 02:54:04 +0100

    Adding Setters and Getters

--- Person.java
+++ Person.java
@@ -1,29 +1,55 @@
 public class Person {
     
-    //attributes
+    //Attributes of the Class
+
     private int age;
     private String name;
     private String code;
 
-    //constructors
-    //Default constructor
+    //Constructors
+
+    //Default Constructor
     public Person () {
         this.age = 0;
         this.name = "";
         this.code = "";
     }
 
-    //Constructor with parameters
+    //Constructor with parameters 
     public Person (int age, String name, String code) {
         this.age = age;
         this.name = name;
         this.code = code;
     }
 
+    //Getters and Setters
 
+    //Getters
 
-    public static void main(String [ ] args) {
-        System.out.println("Lol");
+    public int getAge () {
+        return this.age;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public String getCode () {
+        return this.code;
+    }
+
+    //Setters
+
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setCode (String code) {
+        this.code = code;
     }
 
 }
