2ae7267b7e1bada506608986faa7cde43d62152d
flambok <amine@flambok.com>
2017-12-02 02:37:20 +0100

    Initial commit, Person Class

---
+++ Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    
+    //attributes
+    private int age;
+    private String name;
+    private String code;
+
+    //constructors
+    //Default constructor
+    public Person () {
+        this.age = 0;
+        this.name = "";
+        this.code = "";
+    }
+
+    //Constructor with parameters
+    public Person (int age, String name, String code) {
+        this.age = age;
+        this.name = name;
+        this.code = code;
+    }
+
+
+
+    public static void main(String [ ] args) {
+        System.out.println("Lol");
+    }
+
+}
+
