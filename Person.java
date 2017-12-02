12c9086c48f6ba457ccd16b5257d37ffd6ced1bd
flambok <amine@flambok.com>
2017-12-02 04:26:52 +0100

    Correcting comments

--- Person.java
+++ Person.java
@@ -34,25 +34,25 @@ public class Person {
 
     //Id Getter
     public int getId () {
-        System.out.println("Calling the Id Getter for the persone : " + this.id);
+        System.out.println("Calling the Id Getter for the person : " + this.id);
         return this.id;
     }
 
     //Age Getter
     public int getAge () {
-        System.out.println("Calling the Age Getter for the persone : " + this.id);
+        System.out.println("Calling the Age Getter for the person : " + this.id);
         return this.age;
     }
 
     //Name Getter
     public String getName () {
-        System.out.println("Calling the Name Getter for the persone : " + this.id);
+        System.out.println("Calling the Name Getter for the person : " + this.id);
         return this.name;
     }
 
     //Code Getter
     public String getCode () {
-        System.out.println("Calling the Code Getter for the persone : " + this.id);
+        System.out.println("Calling the Code Getter for the person : " + this.id);
         return this.code;
     }
 
@@ -60,25 +60,25 @@ public class Person {
     
     //Id Setter
     public void setId (int id) {
-        System.out.println("Calling the Id Setter for the persone : " + this.id);
+        System.out.println("Calling the Id Setter for the person : " + this.id);
         this.id = id;
     }
 
     //Age Setter
     public void setAge (int age) {
-        System.out.println("Calling the Age Setter for the persone : " + this.id);
+        System.out.println("Calling the Age Setter for the person : " + this.id);
         this.age = age;
     }
     
     //Name Setter
     public void setName (String name) {
-        System.out.println("Calling the Name Setter for the persone : " + this.id);
+        System.out.println("Calling the Name Setter for the person : " + this.id);
         this.name = name;
     }
     
     //Code Setter
     public void setCode (String code) {
-        System.out.println("Calling the Code Setter for the persone : " + this.id);
+        System.out.println("Calling the Code Setter for the person : " + this.id);
         this.code = code;
     }
 
@@ -93,19 +93,19 @@ public class Person {
 
     //Introduce Method
     public void introduce() {
-        System.out.println("Calling the Introduce Method for the persone : " + this.id);
+        System.out.println("Calling the Introduce Method for the person : " + this.id);
         System.out.println("Hello, I am " + this.name + " and I am really happy to meet you !");
     }
 
     //Ask Method
     public void ask() {
-        System.out.println("Calling the Ask Method for the persone : " + this.id);
+        System.out.println("Calling the Ask Method for the person : " + this.id);
         System.out.println("Is it possible to do that when we are " + this.age + " ?");
     }
     
     //ToString Method
     public String toString() {
-        System.out.println("Calling the ToString Method for the persone : " + this.id);
+        System.out.println("Calling the ToString Method for the person : " + this.id);
         return this.id + " : This is " + this.name + ", he has " + this.age + " years old, and his code is : " + this.code;
     }
 
